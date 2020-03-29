a43b5b308430fefa3add0ac27d66a2dfa8f94264
rutuja kalyane <you@example.com>
2020-03-29 18:27:22 +0530

    ADDED : Calculate Monthly wage of employee

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,15 +1,20 @@
 EmpRatePerHour=20
-check=$((RANDOM%3))
-case $check in
-	1)
-		TotalHour=8
-		;;
-	2)
-		TotalHour=4
-		;;
-	*)
-		TotalHour=0
-		;;
+DaysPerMonth=20
+for (( i=1;i<=DaysPerMonth;i++ ))
+do
+	check=$((RANDOM%3))
+	case $check in
+		1)
+			TotalHour=8
+			;;
+		2)
+			TotalHour=4
+			;;
+		*)
+			TotalHour=0
+			;;
 esac
-sal=$(($EmpRatePerHour * $TotalHour))
-echo "salary of employee is : $sal"
+SalaryPerDay=$(($EmpRatePerHour * $TotalHour))
+MonthlySalary=$(($MonthlySalary+$SalaryPerDay))
+done
+echo "Monthly salary of employee is : $MonthlySalary"
