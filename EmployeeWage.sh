bc9ced6a521d6ec2ca838555d6255c7342011248
rutuja kalyane <you@example.com>
2020-03-29 17:47:50 +0530

    ADDED : Add part time employee and wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,11 +1,10 @@
-check=$(($RANDOM%2))
+EmpRatePerHour=20
+check=$((RANDOM%3))
 if [ $check -eq 1 ]
-	then 
-		WagePerHour=20
-		EmpHour=8
-		salary=$(($WagePerHour*$EmpHour))
-		
-	else
-		salary=0
-	fi
-	echo "Daily slary of Employee is : $salary"
+then 
+	TotalHour=8
+else [ $check -eq 2 ]
+	TotalHour=4
+fi
+sal=$(($EmpRatePerHour*$TotalHour))
+echo "Salary of employee is : $sal"
